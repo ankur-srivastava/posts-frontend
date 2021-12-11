f5a4c02e4173752b66536428dcd50d6699e13990
Ankur Srivastava <ankurlkw@gmail.com>
2021-12-11 07:04:02 +0530

    Added a Login Screen

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # posts-frontend
-Created with CodeSandbox
+
+A frontend for my Posts application built using React.
+
+References
+
+For auth followed this tutorial
+https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import "./styles.css";
 import Header from "./Header";
 import Feed from "./Feed";
 import Footer from "./Footer";
+import Login from "../src/components/Login/Login";
+
+// Tutorial
+import Dashboard from "./components/Dashboard/Dashboard";
+import Preferences from "./components/Preferences/Preferences";
 
 // Header
 // Main
 // Side
 // Footer
 
+// Commenting for Tutorial Work
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Feed />
+//       <Footer />
+//     </div>
+//   );
+// }
+
 export default function App() {
+  const [token, setToken] = useState();
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
   return (
-    <div className="App">
-      <Header />
-      <Feed />
-      <Footer />
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
